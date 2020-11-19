a2c80c849bc3a8812f6b7cee7eab7f9ac987c425
davas93 <vasin.da93@gmail.com>
2020-11-19 16:55:10 +0300

    script.js -> commands: alert and comsole.log

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ let addExpenses = 200000;
 let deposit = 100000;
 let mission = 'Заработать 2000000';
 let period = 'до 31 декабря';
+
+alert(money);
+alert(income);
+alert(addExpenses);
+alert(deposit);
+alert(mission);
+alert(period);
+
+console.log(money);
+console.log(income);
+console.log(addExpenses);
+console.log(deposit);
+console.log(mission);
+console.log(period);
