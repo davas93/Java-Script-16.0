b9c714472892d85fb129bf47cdcbe730a9afa316
davas93 <vasin.da93@gmail.com>
2020-11-27 16:31:12 +0300

    checkpoint

--- js/script.js
+++ js/script.js
@@ -1,48 +1,54 @@
-let isNumber = function (n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
-let money,
-  income = "Сторонние заказы",
-  addExpenses = prompt("Перечислите возможные расходы через запятую?"),
-  deposit = confirm("Есть ли у Вас депозит в банке"),
-  mission = 200000,
-  period = 5;
+"use strict";
 
-let start = function () {
-  do {
-    money = prompt("Ваш месячный доход?");
-  } while (!isNumber(money));
-};
+let money,
+  expenses = [],
+  isNumber = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  },
+  start = function () {
+    do {
+      money = prompt("Ваш месячный доход?");
+    } while (!isNumber(money));
+  },
+  showTypeOf = function (item) {
+    console.log(typeof item);
+  },
+  getExpensesMonth = function () {
+    let sum = 0;
+
+    for (let i = 0; i < 2; i++) {
+      expenses[i] = prompt("Введите обязательную статью расходов");
+
+      let checkNumber;
+
+      do {
+        checkNumber = prompt("Во сколко это обойдется?");
+      } while (!isNumber(checkNumber));
+      sum += +checkNumber;
+    }
+    return sum;
+  };
 
 start();
 
-let showTypeOf = function (item) {
-  console.log(typeof item);
+let appData = {
+  income: {},
+  addIncome: [],
+  expenses: {},
+  addExpenses: [],
+  deposit: false,
+  mission: 200000,
+  period: 5,
+  asking: function () {
+    let addExpenses = prompt("Перечислите возможные расходы через запятую?");
+    appData.addExpenses = addExpenses.toLowerCase().split(",");
+    app.Data.deposit = confirm("Есть ли у Вас депозит в банке");
+  },
 };
+
 showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-
-let expenses = [];
-
-console.log(addExpenses.toLowerCase().split(","));
-
-let getExpensesMonth = function () {
-  let sum = 0;
-
-  for (let i = 0; i < 2; i++) {
-    expenses[i] = prompt("Введите обязательную статью расходов");
-
-    let checkNumber;
-
-    do {
-      checkNumber = prompt("Во сколко это обойдется?");
-    } while (!isNumber(checkNumber));
-    sum += +checkNumber;
-  }
-  console.log(expenses);
-  return sum;
-};
+showTypeOf(appData.income);
+showTypeOf(appData.deposit);
 
 let expensesAmonth = getExpensesMonth();
 
@@ -55,7 +61,7 @@ let getAccumulatedMonth = function () {
 let accumulatedMonth = getAccumulatedMonth();
 
 let getTargetMonth = function () {
-  let target = mission / accumulatedMonth;
+  let target = appData.mission / accumulatedMonth;
   if (target < 0) {
     console.log("Цель не будет достигнута");
   } else {
