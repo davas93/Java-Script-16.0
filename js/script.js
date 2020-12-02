d6b0a4b72b765df85d89b04b642699207b8bcd59
davas93 <vasin.da93@gmail.com>
2020-12-02 14:28:46 +0300

    item3 complete

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 const book = document.querySelectorAll(".book");
 const books = document.querySelector(".books");
 const body = document.querySelector("body");
+const titleBook3 = book[4].querySelector("a");
 
 //Переставляем книги
 books.prepend(book[1]);
@@ -12,4 +13,7 @@ book[3].before(book[4]);
 //Меняем фон
 body.style.backgroundImage = "url(./image/you-dont-know-js.jpg)";
 
-console.log(body);
+//Исправляем заголовок
+titleBook3.textContent = "Книга 3. this и Прототипы Объектов";
+
+console.log(titleBook3);
