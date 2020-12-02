daeb877df48edf699ba27b5718bf5f310e006051
davas93 <vasin.da93@gmail.com>
2020-12-02 14:23:40 +0300

    item2 complete

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@
 
 const book = document.querySelectorAll(".book");
 const books = document.querySelector(".books");
+const body = document.querySelector("body");
 
+//Переставляем книги
 books.prepend(book[1]);
 books.append(book[2]);
 book[3].before(book[4]);
 
-console.log(book);
+//Меняем фон
+body.style.backgroundImage = "url(./image/you-dont-know-js.jpg)";
+
+console.log(body);
