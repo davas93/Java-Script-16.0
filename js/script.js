92e5f23912caf7f9e6aa4675e0d43a5f2576c6e6
davas93 <vasin.da93@gmail.com>
2020-12-02 14:17:29 +0300

    item1 coplete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-'use strict'
+"use strict";
+
+const book = document.querySelectorAll(".book");
+const books = document.querySelector(".books");
+
+books.prepend(book[1]);
+books.append(book[2]);
+book[3].before(book[4]);
+
+console.log(book);
