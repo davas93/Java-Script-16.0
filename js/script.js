983bfd3e78378ac3d2adbc574ba4b95a2bcb9b89
davas93 <vasin.da93@gmail.com>
2020-12-02 14:00:35 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@ let calculateBtn = document.getElementById("start"),
   expensesAddBtn = document.getElementsByTagName("button")[1],
   depositCheck = document.querySelector("#deposit-check"),
   incomeInputs = document.querySelectorAll(".additional_income-item"),
-  budgetDayValue = document.getElementsByClassName("budget_day-value"),
-  expensesMonthValue = document.getElementsByClassName("expenses_month-value"),
+  budgetDayValue = document.getElementsByClassName("budget_day-value")[0],
+  expensesMonthValue = document.getElementsByClassName("expenses_month-value")[0],
   additionalIncomeValue = document.getElementsByClassName(
     "additional_income-value"
-  ),
+  )[0],
   additionalExpensesValue = document.getElementsByClassName(
     "additional_expenses-value"
-  ),
-  incomePeriodValue = document.getElementsByClassName("income_period-value"),
-  targetMonthValue = document.getElementsByClassName("target_month-value"),
+  )[0],
+  incomePeriodValue = document.getElementsByClassName("income_period-value")[0],
+  targetMonthValue = document.getElementsByClassName("target_month-value")[0],
   budgetMonthValue = document.querySelector(".budget_month-value"),
   incomeMonthInput = document.querySelector(".salary-amount"),
   incomeNameInput = document.querySelector(".income-title"),
@@ -24,7 +24,7 @@ let calculateBtn = document.getElementById("start"),
   additionalExpensesInput = document.querySelector(".additional_expenses-item"),
   targetAmountInput = document.querySelector(".target-amount"),
   periodSelect = document.querySelector(".period-select"),
-  periodNum = document.querySelector("period-amount");
+  periodNum = document.querySelector(".period-amount");
 
 console.log(
   calculateBtn,
