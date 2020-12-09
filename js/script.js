ca263798e77768019c26315be76e5141d3da7c3a
davas93 <vasin.da93@gmail.com>
2020-12-09 17:33:26 +0300

    complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+class First {
+  hello() {
+    console.log("Привет, я метод родителя!");
+  }
+}
+
+class Second extends First {
+  hello() {
+    return super.hello() + console.log("А я наследуемый метод");
+  }
+}
+
+const first = new First();
+const second = new Second();
+second.hello();
